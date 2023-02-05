c0975645f0dc5d3568da734f8e9e82e57c609e01
1289rew <aaolga7a@gmail.com>
2023-02-05 22:20:53 +0300

    Сообщества

---
+++ yatube/posts/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import get_object_or_404, render
+
+from .models import Group, Post
+
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
